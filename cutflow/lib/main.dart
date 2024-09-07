d8968fde09b93cf0d5d4324c4f6a79521bd776c5
judicaelKos <judicael.damwork@gmail.com>
2024-09-07 11:22:24 +0200

    init firebase

--- cutflow/lib/main.dart
+++ cutflow/lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:cutflow/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_glow/flutter_glow.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,12 +25,14 @@ class MyApp extends StatelessWidget {
           Theme.of(context).textTheme,
         ),
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -37,31 +47,35 @@ class _HomePageState extends State<HomePage> {
   void _scrollToCategory(GlobalKey key) {
     final context = key.currentContext;
     if (context != null) {
-      Scrollable.ensureVisible(context, duration: Duration(seconds: 1), curve: Curves.easeInOut);
+      Scrollable.ensureVisible(context,
+          duration: const Duration(seconds: 1), curve: Curves.easeInOut);
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFA391CB),
+      backgroundColor: const Color(0xFFA391CB),
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Color(0xFFA391CB),
-        title: Text('CutFlow'),
+        backgroundColor: const Color(0xFFA391CB),
+        title: const Text('CutFlow'),
         actions: MediaQuery.of(context).size.width >= 600
             ? [
                 TextButton(
                   onPressed: () => _scrollToCategory(_category1Key),
-                  child: Text("Catégorie 1", style: TextStyle(color: Colors.white)),
+                  child: const Text("Catégorie 1",
+                      style: TextStyle(color: Colors.white)),
                 ),
                 TextButton(
                   onPressed: () => _scrollToCategory(_category2Key),
-                  child: Text("Jeux", style: TextStyle(color: Colors.white)),
+                  child:
+                      const Text("Jeux", style: TextStyle(color: Colors.white)),
                 ),
                 TextButton(
                   onPressed: () => _scrollToCategory(_category3Key),
-                  child: Text("Formules", style: TextStyle(color: Colors.white)),
+                  child: const Text("Formules",
+                      style: TextStyle(color: Colors.white)),
                 ),
               ]
             : null,
@@ -71,7 +85,7 @@ class _HomePageState extends State<HomePage> {
               child: ListView(
                 padding: EdgeInsets.zero,
                 children: [
-                  DrawerHeader(
+                  const DrawerHeader(
                     decoration: BoxDecoration(
                       color: Color(0xFF6C5CE7),
                     ),
@@ -84,21 +98,21 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   ListTile(
-                    title: Text('Catégorie 1'),
+                    title: const Text('Catégorie 1'),
                     onTap: () {
                       Navigator.pop(context);
                       _scrollToCategory(_category1Key);
                     },
                   ),
                   ListTile(
-                    title: Text('Jeux'),
+                    title: const Text('Jeux'),
                     onTap: () {
                       Navigator.pop(context);
                       _scrollToCategory(_category2Key);
                     },
                   ),
                   ListTile(
-                    title: Text('Formules'),
+                    title: const Text('Formules'),
                     onTap: () {
                       Navigator.pop(context);
                       _scrollToCategory(_category3Key);
@@ -137,7 +151,7 @@ class Category1 extends StatelessWidget {
   final String title;
   final String content;
 
-  Category1({
+  const Category1({
     required Key key,
     required this.title,
     required this.content,
@@ -146,30 +160,39 @@ class Category1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16.0),
-      margin: EdgeInsets.symmetric(vertical: 8.0),
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             title,
-            style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold),
+            style:
+                GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 8.0),
-          Container(
+          const SizedBox(height: 8.0),
+          SizedBox(
             height: 200,
             child: CarouselView(
-              overlayColor: WidgetStateProperty.all(Colors.black.withOpacity(0.5)),
+              overlayColor:
+                  WidgetStateProperty.all(Colors.black.withOpacity(0.5)),
               shrinkExtent: 150,
               itemExtent: 400,
               onTap: (value) => debugPrint("Carousel item tapped: $value"),
               children: [
-                _buildCarouselItem(context, "assets/images/lol.jpg", "League of Legends", isAvalable: true),
-                _buildCarouselItem(context, "assets/images/valorant-305kescxw5dpup7y.jpg", "Valorant"),
-                _buildCarouselItem(context, "assets/images/lol.jpg", "League of Legends"),
-                _buildCarouselItem(context, "assets/images/lol.jpg", "League of Legends"),
-                _buildCarouselItem(context, "assets/images/lol.jpg", "League of Legends"),
-                _buildCarouselItem(context, "assets/images/lol.jpg", "League of Legends"),
+                _buildCarouselItem(
+                    context, "assets/images/lol.jpg", "League of Legends",
+                    isAvalable: true),
+                _buildCarouselItem(context,
+                    "assets/images/valorant-305kescxw5dpup7y.jpg", "Valorant"),
+                _buildCarouselItem(
+                    context, "assets/images/lol.jpg", "League of Legends"),
+                _buildCarouselItem(
+                    context, "assets/images/lol.jpg", "League of Legends"),
+                _buildCarouselItem(
+                    context, "assets/images/lol.jpg", "League of Legends"),
+                _buildCarouselItem(
+                    context, "assets/images/lol.jpg", "League of Legends"),
               ],
             ),
           )
@@ -178,13 +201,16 @@ class Category1 extends StatelessWidget {
     );
   }
 
-  Widget _buildCarouselItem(BuildContext context, String path, String text, {bool isAvalable = false}) {
+  Widget _buildCarouselItem(BuildContext context, String path, String text,
+      {bool isAvalable = false}) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12.0),
         image: DecorationImage(
-          colorFilter:
-              isAvalable == false ? ColorFilter.mode(Color(0xFF6C5CE7).withOpacity(0.5), BlendMode.srcOver) : null,
+          colorFilter: isAvalable == false
+              ? ColorFilter.mode(
+                  const Color(0xFF6C5CE7).withOpacity(0.5), BlendMode.srcOver)
+              : null,
           image: AssetImage(path),
           fit: BoxFit.cover,
         ),
@@ -192,7 +218,8 @@ class Category1 extends StatelessWidget {
       child: Center(
           child: Text(
         text,
-        style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+        style: GoogleFonts.poppins(
+            fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
       )),
     );
   }
@@ -203,7 +230,7 @@ class Category2 extends StatelessWidget {
   final String content;
   final bool isCarousel;
 
-  Category2({
+  const Category2({
     required Key key,
     required this.title,
     required this.content,
@@ -213,14 +240,15 @@ class Category2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16.0),
-      margin: EdgeInsets.symmetric(vertical: 8.0),
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             title,
-            style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold),
+            style:
+                GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold),
           ),
         ],
       ),
@@ -229,7 +257,7 @@ class Category2 extends StatelessWidget {
 }
 
 class PricingCategorySection extends StatefulWidget {
-  PricingCategorySection({Key? key}) : super(key: key);
+  const PricingCategorySection({super.key});
 
   @override
   _PricingCategorySectionState createState() => _PricingCategorySectionState();
@@ -248,14 +276,15 @@ class _PricingCategorySectionState extends State<PricingCategorySection> {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      padding: EdgeInsets.all(16.0),
-      margin: EdgeInsets.symmetric(vertical: 8.0),
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
       child: Column(
         children: [
           Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             Text(
               "Nos Formules",
-              style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold),
+              style: GoogleFonts.poppins(
+                  fontSize: 24, fontWeight: FontWeight.bold),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -263,8 +292,11 @@ class _PricingCategorySectionState extends State<PricingCategorySection> {
                 GlowText(
                   "Mensuel",
                   blurRadius: 0,
-                  offset: Offset(0, 0),
-                  style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xFF6C5CE7)),
+                  offset: const Offset(0, 0),
+                  style: GoogleFonts.poppins(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFF6C5CE7)),
                 ),
                 const SizedBox(width: 16.0),
                 Switch(
@@ -279,21 +311,24 @@ class _PricingCategorySectionState extends State<PricingCategorySection> {
                 GlowText(
                   "Annuel",
                   blurRadius: 0,
-                  offset: Offset(0, 0),
-                  style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xFF6C5CE7)),
+                  offset: const Offset(0, 0),
+                  style: GoogleFonts.poppins(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFF6C5CE7)),
                 ),
               ],
             ),
           ]),
-          SizedBox(height: 16.0),
+          const SizedBox(height: 16.0),
           const SizedBox(height: 16.0),
           Row(
             children: [
-              _buildPricingCard(
-                  "Plan Basic", _isAnnual ? 100 : 10, "Le plan de base avec toutes les fonctionnalités essentielles."),
+              _buildPricingCard("Plan Basic", _isAnnual ? 100 : 10,
+                  "Le plan de base avec toutes les fonctionnalités essentielles."),
               const SizedBox(width: 16.0),
-              _buildPricingCard(
-                  "Plan Standard", _isAnnual ? 200 : 20, "Le plan standard avec fonctionnalités supplémentaires.",
+              _buildPricingCard("Plan Standard", _isAnnual ? 200 : 20,
+                  "Le plan standard avec fonctionnalités supplémentaires.",
                   isFamous: true),
               const SizedBox(width: 16.0),
               _buildPricingCard(
@@ -308,7 +343,8 @@ class _PricingCategorySectionState extends State<PricingCategorySection> {
     );
   }
 
-  Widget _buildPricingCard(String title, double price, String description, {bool isFamous = false}) {
+  Widget _buildPricingCard(String title, double price, String description,
+      {bool isFamous = false}) {
     return Expanded(
       child: GestureDetector(
         onTap: () {
@@ -320,14 +356,17 @@ class _PricingCategorySectionState extends State<PricingCategorySection> {
               decoration: BoxDecoration(
                 color: Colors.white,
                 shape: BoxShape.rectangle,
-                border: isFamous ? Border.all(color: const Color(0xFF6C5CE7), width: 4) : null,
+                border: isFamous
+                    ? Border.all(color: const Color(0xFF6C5CE7), width: 4)
+                    : null,
                 borderRadius: BorderRadius.circular(12.0),
               ),
               child: ListTile(
-                contentPadding: EdgeInsets.all(16.0),
+                contentPadding: const EdgeInsets.all(16.0),
                 title: Text(
                   title,
-                  style: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.bold),
+                  style: GoogleFonts.poppins(
+                      fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 subtitle: Text(
                   "${price.toStringAsFixed(2)} ${_isAnnual ? '€ / an' : '€ / mois'}\n$description",
@@ -347,7 +386,10 @@ class _PricingCategorySectionState extends State<PricingCategorySection> {
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
                       "Populaire",
-                      style: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                      style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
                   ),
                 ),
